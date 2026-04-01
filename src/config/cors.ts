f382375560e16ae4d88377c4d401b2b2f9caea91
Kolawole Akintayo <kolawoleakintayok@gmail.com>
2026-04-01 06:31:33 +0100

    Add cors domain

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,32 +1,34 @@
 import { CorsOptions } from 'cors';
 
 const allowedOrigins: string[] = [
-    'http://localhost:5173',
-    '*',
+  'http://localhost:5173',
+  'https://marketing.biz360prime.com',
+  'https://biz360prime.com',
+  'https://www.thedm360.com',
 ];
 
 const corsOptions: CorsOptions = {
-    origin: (origin, callback) => {
-        // Check if the origin is undefined (e.g., when testing without a browser)
-        if (!origin) {
-            return callback(null, true);
-        }
+  origin: (origin, callback) => {
+    // Check if the origin is undefined (e.g., when testing without a browser)
+    if (!origin) {
+      return callback(null, true);
+    }
 
-        // For '*' wildcard origin, allow all origins
-        if (allowedOrigins.includes('*')) {
-            return callback(null, true);
-        }
+    // For '*' wildcard origin, allow all origins
+    if (allowedOrigins.includes('*')) {
+      return callback(null, true);
+    }
 
-        // Check if the origin is in the allowedOrigins array
-        if (allowedOrigins.includes(origin)) {
-            return callback(null, true);
-        } else {
-            return callback(new Error('Not allowed by CORS'));
-        }
-    },
-    credentials: true,
-    optionsSuccessStatus: 200,
-    methods: ["GET", "HEAD", "POST", "PUT", "DELETE", "PATCH"],
-}
+    // Check if the origin is in the allowedOrigins array
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  optionsSuccessStatus: 200,
+  methods: ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'PATCH'],
+};
 
 export default corsOptions;
